ea33b202ca561749232dc8f4c98032cd0e5dfaf8
Dioni Mercado <me@dionimercado.com>
2019-05-19 23:36:53 -0400

    Intro improvements and Footer changes

--- footer.php
+++ footer.php
@@ -15,22 +15,37 @@
          </div>
         </div>
 
-         <div class="col-sm-6 col-md-4">
-           <h3 class="footer-title pb-3">contacts</h3>
+         <div class="col-sm-6 col-md-4 mb-5">
+           <h3 class="footer-title pb-3">Inwood</h3>
            <div class="address">
              <p><i class="fa fa-phone"></i><strong>phone: </strong> <a href="tel:<?php echo get_post_meta( get_the_ID(), 'phone', true ) ?>"><?php echo get_post_meta( get_the_ID(), 'phone', true ) ?></a></p>
              <p><i class="fa fa-envelope"></i><strong>email: </strong><a href="mailto:<?php echo get_post_meta( get_the_ID(), 'email', true ) ?>"><?php echo get_post_meta( get_the_ID(), 'email', true ) ?></a></p>
              <p class="icon-map"><i class="fa fa-map-marker"></i><strong>address: </strong><?php echo get_post_meta( get_the_ID(), 'address', true ) ?></p>
-             <!-- <p class="road-details">New York, NY 10034</p> -->
            </div>
-
+           <div class="d-flex">
+             <a href="<?php echo get_post_meta( get_the_ID(), 'gmaps', true ) ?>" target="_blank" class="btn btn-outline-light btn-sm text-uppercase mr-3">Get directions</a>
+             <a href="/inwood/" class="btn btn-outline-light btn-sm text-uppercase">Visit Site</a>
+           </div>
         </div>
-        <div class="col-sm-6 col-md-4">
+
+         <div class="col-sm-6 col-md-4">
+           <h3 class="footer-title pb-3">Harlem</h3>
+           <div class="address">
+             <p><i class="fa fa-phone"></i><strong>phone: </strong> <a href="tel:<?php echo get_post_meta( get_the_ID(), 'harlem_phone', true ) ?>"><?php echo get_post_meta( get_the_ID(), 'harlem_phone', true ) ?></a></p>
+             <p><i class="fa fa-envelope"></i><strong>email: </strong><a href="mailto:<?php echo get_post_meta( get_the_ID(), 'harlem_email', true ) ?>"><?php echo get_post_meta( get_the_ID(), 'harlem_email', true ) ?></a></p>
+             <p class="icon-map"><i class="fa fa-map-marker"></i><strong>address: </strong><?php echo get_post_meta( get_the_ID(), 'harlem_address', true ) ?></p>
+           </div>
+           <div class="d-flex">
+             <a href="<?php echo get_post_meta( get_the_ID(), 'harlem_gmaps', true ) ?>" target="_blank" class="btn btn-outline-light btn-sm text-uppercase mr-3">Get directions</a>
+             <a href="/harlem/" class="btn btn-outline-light btn-sm text-uppercase">Visit Site</a>
+           </div>
+        </div>
+        <!-- <div class="col-sm-6 col-md-4">
           <h3 class="footer-title mt-4 mt-sm-0 pb-4">Get directions</h3>
           <div class="google-map">
             <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12068.844724666318!2d-73.9171311!3d40.8672395!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x724cc82ad64d9f57!2sMade+In+Mexico!5e0!3m2!1sen!2sus!4v1556328731931!5m2!1sen!2sus" width="100%" height="160" frameborder="0" style="border:0" allowfullscreen></iframe>
           </div>
-        </div>
+        </div> -->
       </div>
     </div>
   </div>
@@ -38,7 +53,7 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-8">
-          <p class="copyright">
+          <p class="copyright text-white-50">
             &copy; Copyright <?php echo date("Y"); _e(' Made In Mexico. ') ?> <span class="d-none d-md-inline-block">All Rights Reserved</span>
           </p>
         </div>
